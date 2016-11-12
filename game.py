27948c73baffdc28db4402ea1e1b6c6b6d75e885
gregovin <gregory.w.hess@gmail.com>
2016-11-11 19:09:02 -0800

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,20 @@
 import pygame
+import math
+def atan3(y, x):
+   if x==0 and y==0:
+      return 0
+   elif x==0 and y>0:
+      return math.pi/2
+   elif x==0:
+      return -math.pi/2
+   else:
+      return math.atan2(y, x)
 player = [200, 200, 0, 0, 0]
 class Enimy:
+   def update(self):
+      self.pos[0] = self.pos[0] + self.vel[0]
+      self.pos[1] = self.pos[1] + self.vel[0]
+      
    def __init__(self, poses, vels, rota):
       self.pos = poses
       self.vel = vels
